d2c39bda30f9abc8c61001ff1d866cfa9d74dc6e
Mohamed Amine Belabbes <medamine@Debras-macOS.local>
2021-06-02 14:57:28 +0100

    Annotate tweets

--- query_tweets.py
+++ query_tweets.py
@@ -3,28 +3,41 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-auth = tweepy.OAuthHandler(os.environ.get("CONSUMER_KEY"), os.environ.get("CONSUMER_SECRET"))
-auth.set_access_token(os.environ.get("ACCESS_TOKEN"), os.environ.get("ACCESS_TOKEN_SECRET"))
-api = tweepy.API(auth)
+def twitter_auth():
+    auth = tweepy.OAuthHandler(os.environ.get("CONSUMER_KEY"), os.environ.get("CONSUMER_SECRET"))
+    auth.set_access_token(os.environ.get("ACCESS_TOKEN"), os.environ.get("ACCESS_TOKEN_SECRET"))
+    return auth
 
-obj_ = api.statuses_lookup([665257826961530880,
-665257828479860736,
-665257828941209600,
-665257832170848257,
-665257832590278656,
-665257833500426241,
-665257835144601600,
-665257838290292736,
-665257840844652544,
-665257846020440064,
-665257847337443330,
-665257848457330689])
+def get_api(auth):
+    api = tweepy.API(auth)
+    return api
 
-with open("sample.ids", "w") as annotated:
-    for tweet in obj_:
-        print("ID: ", tweet.id_str)
-        print("Tweet: ", tweet.text)
-        annotation = input("Annotate (0: Not relevant, 1: Relevant, 2: Informative): ")
-        if int(annotation) < 0 or int(annotation) > 2:
-            raise Exception("Invalid annotation: ", annotation)
-        annotated.write(tweet.id_str + "," + annotation + "\n")
+def lookup_tweets(tweet_ids, api):
+    status = api.statuses_lookup(tweet_ids)
+    return status
+
+def annotate_tweets(tweet_ids, event_name, start_index, api):
+    status = lookup_tweets(tweet_ids, api)
+    with open(event_name + "-" + str(start_index), "w") as annotated:
+        for tweet in status:
+            print("ID: ", tweet.id_str)
+            print("Tweet: ", tweet.text)
+            annotation = input("Annotate (0: Not relevant, 1: Relevant, 2: Informative): ")
+            if int(annotation) < 0 or int(annotation) > 2:
+                annotation = input("Try again: ")
+            annotated.write(tweet.id_str + "," + annotation + "\n")
+    return True
+
+def main():
+    filename = input("Enter filename: ")
+    start_index = int(input("Enter start index: "))
+    tweet_ids = []
+    with open(filename) as event_tweets:
+        event_tweets_ids = event_tweets.readlines()
+        for i in range(int(start_index), int(start_index)+100):
+            tweet_ids.append(int(event_tweets_ids[i]))
+    auth = twitter_auth()
+    api = get_api(auth)
+    annotate_tweets(tweet_ids, filename, start_index, api)
+
+main()
